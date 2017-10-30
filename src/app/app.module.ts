c0b517c2586307ceaf11e5ab4ceed9a2ef181c57
David Drace <david@drace.net>
2017-10-30 13:16:14 -0500

    organization edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail.component';
-import { RecipeItemcdComponent } from './recipe-list/recipe-itemcd/recipe-itemcd.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recipe-detail.component';
+
+import { RecipesComponent } from './recipes/recipes.component';
 
 
 @NgModule({
@@ -20,7 +21,7 @@ import { RecipeItemcdComponent } from './recipe-list/recipe-itemcd/recipe-itemcd
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeItemcdComponent
+    RecipesComponent
   ],
   imports: [
     BrowserModule
